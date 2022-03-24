25d9b4af5a175ab399185c6fc85c95006ba96b1f
dewunmirokeeb <adedokunadewunmi60@gmail.com>
2022-03-24 10:44:51 +0100

    todo_service and user_service done

--- lib/services/todoservice.dart
+++ lib/services/todoservice.dart
@@ -1,3 +1,48 @@
+import 'package:eventscheduler/database/tododatabase.dart';
+import 'package:eventscheduler/models/todo.dart';
 import 'package:flutter/material.dart';
 
-class TodoService with ChangeNotifier {}
+class TodoService with ChangeNotifier {
+  List<Todo> _todos = [];
+  List<Todo> get todos => _todos;
+
+  Future<String> gettodos(String username) async {
+    try {
+      _todos = await TodoDataBase.instance.gettodo(username);
+      notifyListeners();
+    } catch (e) {
+      return e.toString();
+    }
+    return 'ok';
+  }
+
+  Future<String> deletetodo(Todo todo) async {
+    try {
+      await TodoDataBase.instance.deleteTodo(todo);
+    } catch (e) {
+      return e.toString();
+    }
+    String result = await gettodos(todo.username);
+    return result;
+  }
+
+  Future<String> createtodo(Todo todo) async {
+    try {
+      await TodoDataBase.instance.createTodo(todo);
+    } catch (e) {
+      return e.toString();
+    }
+    String result = await gettodos(todo.username);
+    return result;
+  }
+
+  Future<String> toggletododone(Todo todo) async {
+    try {
+      await TodoDataBase.instance.toggletododdoing(todo);
+    } catch (e) {
+      return e.toString();
+    }
+    String result = await gettodos(todo.username);
+    return result;
+  }
+}
--- lib/services/user_service.dart
+++ lib/services/user_service.dart
@@ -1,13 +1,73 @@
+import 'package:eventscheduler/database/tododatabase.dart';
+import 'package:eventscheduler/models/user.dart';
 import 'package:flutter/material.dart';
 
 class UserService with ChangeNotifier {
-  String getHumanReadableError(String message) {
-    if (message.contains('UNIQUE constraint fail')) {
-      return 'This user already exist in the data base, please choose a new user name';
-    }
-    if (message.contains('not found in the data ase')) {
-      return 'The user does not exist in the database. please regster first';
-    }
-    return message;
+  late User _currentuser;
+  bool _busycreate = false;
+  bool _userexist = false;
+
+  User get currentuser => _currentuser;
+  bool get busycreate => _busycreate;
+  bool get userexist => _userexist;
+
+  set userexist(bool value) {
+    _userexist = value;
+    notifyListeners();
   }
+
+  Future<String> getuser(String username) async {
+    String result = 'ok';
+    try {
+      _currentuser = await TodoDataBase.instance.getUser(username);
+    } catch (e) {
+      result = getHumanReadableError(e.toString());
+    }
+    return result;
+  }
+
+  Future<String> checkifuserexist(String username) async {
+    String result = 'ok';
+    try {
+      await TodoDataBase.instance.getUser(username);
+    } catch (e) {
+      result = getHumanReadableError(e.toString());
+    }
+    return result;
+  }
+
+  Future<String> updatethecurrentuser(String username) async {
+    String result = 'ok';
+    _currentuser.name = username;
+    notifyListeners();
+    try {
+      await TodoDataBase.instance.updateUser(_currentuser);
+    } catch (e) {
+      result = getHumanReadableError(e.toString());
+    }
+    return result;
+  }
+
+  Future<String> createuser(User user) async {
+    String result = 'ok';
+    _busycreate = true;
+    notifyListeners();
+    try {
+      await TodoDataBase.instance.createuser(user);
+    } catch (e) {
+      result = getHumanReadableError(e.toString());
+    }
+    _busycreate = false;
+    return result;
+  }
+}
+
+String getHumanReadableError(String message) {
+  if (message.contains('UNIQUE constraint fail')) {
+    return 'This user already exist in the data base, please choose a new user name';
+  }
+  if (message.contains('not found in the data ase')) {
+    return 'The user does not exist in the database. please regster first';
+  }
+  return message;
 }
